34639a5c726fe43625391f86cae79531c468039c
yajuvendrarawat <43192654+yajuvendrarawat@users.noreply.github.com>
2024-05-07 16:38:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
 def main():
-    st.title("Basic Streamlit App")
-    st.write("Welcome to your first Streamlit app!")
+    load_dotenv()
+    # Initialize session state
+    initialize_session_state()
+    st.title("Multi-Docs ChatBot using llama2 :books:")
+    # Initialize Streamlit
+    st.sidebar.title("Document Processing")
+    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)    
 
 if __name__ == "__main__":
     main()
