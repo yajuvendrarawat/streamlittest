66ba47b9eb8dd822510edfb579b17bad5835d481
yajuvendrarawat <43192654+yajuvendrarawat@users.noreply.github.com>
2024-05-09 14:38:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.llms import CTransformers
-from langchain.llms import Replicate
+#from langchain.embeddings import HuggingFaceInstructEmbeddings
+#from langchain.llms import CTransformers
+#from langchain.llms import Replicate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
@@ -13,15 +13,13 @@ from langchain.document_loaders import TextLoader
 from langchain.document_loaders import Docx2txtLoader
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.prompts import PromptTemplate
-from langchain.memory import ConversationBufferMemory
 from langchain.llms import HuggingFacePipeline
-from langchain.chains import RetrievalQA
+#from langchain.chains import RetrievalQA
 
 import torch
 import os
 from dotenv import load_dotenv
 import tempfile
-# from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
